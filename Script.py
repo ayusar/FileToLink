1337434d07753a09178fce42892739e762156455
ayusar <work38896@gmail.com>
2025-10-06 20:32:42 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -6,9 +6,13 @@
 class script(object):
     START_TXT = """<b>Hello {} 👋, My Name Is <a href=https://t.me/{}>{}</a>
     
-✏️ I Am A Advance File Stream Bot With Multiple Player Support And Url Shortner. Best Ui Performance.
+✏️ I Am A Advance File Stream Bot With Multiple Player Support And Url Shortner.
 
-Now Send Me A Media To See Magic ✨</b>"""
+Now Send Me A Media That You Want To Stream/Download And Then See Magic ✨
+
+💥Made with love by - @SmartTgBots
+
+⚡Any query / questions ask on @Smarttgsupportbot </b>"""
 
     LOG_TEXT_P = """#NewUser
 ID - <code>{}</code>
