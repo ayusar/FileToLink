076211cfa9c5ec1ca663c5057eb5fb7bbac92424
ayusar <work38896@gmail.com>
2025-10-18 09:16:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,7 +7,7 @@ id_pattern = re.compile(r'^.\d+$')
 SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', '20916942'))
 API_HASH = environ.get('API_HASH', 'f016114b0ff593ef85429a1b967a138a')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+BOT_TOKEN = environ.get('BOT_TOKEN', "8332321630:AAGrkGUws20muPFQ4muvinRD7mAPctvysb8")
 
 # Bot settings
 PORT = environ.get("PORT", "8080")
@@ -23,14 +23,14 @@ else:
 URL = environ.get("URL", "")
 
 # Admins, Channels & Users
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003046424185'))
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '8147771397').split()]
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003072099994'))
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '8287406589').split()]
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://smartautomationby2010_db_user:Bestsmart@cluster0.ziwakob.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjautobot")
 
 # Shortlink Info
-SHORTLINK = bool(environ.get('SHORTLINK', True)) # Set True Or False
+SHORTLINK = bool(environ.get('SHORTLINK', False)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
 SHORTLINK_API = environ.get('SHORTLINK_API', '2zQzmsHTt9fBUZ6dQxtLUk4m38e2')
